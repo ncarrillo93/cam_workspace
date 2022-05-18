c12bb638ae8be5ecdcef6cf132e3dcd0873c874e
Nicolas <nicolascarrillo33@gmail.com>
2022-05-18 16:43:34 -0400

    avance opencv_aruco.py

--- opencv_aruco.py
+++ opencv_aruco.py
@@ -8,14 +8,25 @@ def findaruco(img,marker_size=6,total_markers=250,draw=True):
     arucoDict=aruco.Dictionary_get(key)
     arucoParam=aruco.DetectorParameters_create()
     bbox,ids,_=aruco.detectMarkers(gray,arucoDict,parameters=arucoParam)
-    print(ids)
     if draw:
-        aruco.drawMarkers(img,bbox)
+        aruco.drawDetectedMarkers(img,bbox)
     return bbox,ids
 
-#from pupil_apriltags as apriltags
+#def coords(bbox):
+#    for i in 
 img = cv2.imread('singlemarkerssource.png')
-img = cv2.resize(img,(1920,1080))
+img = cv2.resize(img,(752,480))
 bbox,ids = findaruco(img)
-cv2.imshow('test tags',img)
-cv2.waitKey(0)
+aux=bbox
+for i in range(0,6
+):
+    print(bbox[i][0][0],bbox[i][0][1],bbox[i][0][2],bbox[i][0][3])
+
+
+# Green color in BGR
+color = (0,0,255)  
+# Line thickness of 9 px
+#thickness = 9
+#image = cv2.line(img, (432,349), (500,349), color, thickness)
+#cv2.imshow('test tags',img)
+#cv2.waitKey(0)
