6d98bb1730fcd20b3da6511488ce972b8cc147b4
Nicolas <nicolascarrillo33@gmail.com>
2022-05-19 16:27:44 -0400

    obtención de coordenadas completada. Falta agregar tutorial al readme

--- opencv_aruco.py
+++ opencv_aruco.py
@@ -3,13 +3,13 @@ import cv2
 import cv2.aruco as aruco
 import numpy as np
 
-class corner:
+class Corner:
     x=0
     y=0
     def __init__(self,x,y):
         self.x=x
         self.y=y
-class tag:
+class Tag:
     corner1=None
     corner2=None
     corner3=None
@@ -29,18 +29,32 @@ def findaruco(img,marker_size=6,total_markers=250,draw=True):
     if draw:
         aruco.drawDetectedMarkers(img,bbox)
     return bbox,ids
-    
-#def coords(bbox):
-#    for i in 
+
+def coords(bbox):
+    tags_array=[]
+    aux=[]
+    corner_aux=Corner(0,0)
+    for i in range(0,len(bbox)):
+        for j in range(0,4):
+            corner_aux.x=bbox[i][0][j][0]
+            corner_aux.y=bbox[i][0][j][1]
+            aux.append(corner_aux)
+        tag_aux=Tag(aux[0],aux[1],aux[2],aux[3])
+        tags_array.append(tag_aux)
+    return tags_array
+
+
 img = cv2.imread('singlemarkerssource.png')
 img = cv2.resize(img,(752,480))
 bbox,ids = findaruco(img)
-aux=[]
-for i in range(0,len(bbox)):
-    aux.append([bbox[i][0][0],bbox[i][0][1],bbox[i][0][2],bbox[i][0][3]])
-    print(i,'  ',bbox[i][0][0],bbox[i][0][1],bbox[i][0][2],bbox[i][0][3])
+tags_array=coords(bbox)
+
+
+
+
+
 # Green color in BGR
-color = (0,0,255)  
+#color = (0,0,255)  
 # Line thickness of 9 px
 #thickness = 9
 #image = cv2.line(img, (432,349), (500,349), color, thickness)
