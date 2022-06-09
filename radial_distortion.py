c70182c9e69792118fe5d3bc6e9a4b63dca40140
Nicolas <nicolascarrillo33@gmail.com>
2022-06-09 18:15:03 -0400

    first try in self-calibration radial distortion

---
+++ radial_distortion.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2 as cv
+import glob
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+a,b=9,6
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+images = glob.glob('img/chessboard/*.jpg')
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(img, (a,b), None)
+    # If found, add object points, image points (after refining them)
+    print(ret)
+    if ret == True:
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray,corners, (a,b), (-1,-1), criteria)
+        imgpoints.append(corners)
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (a,b), corners2, ret)
+        cv.imshow('img', cv.resize(img,(600,400)))
+        cv.waitKey(1000)
+cv.destroyAllWindows()
