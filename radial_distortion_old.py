b1dfc7e94ae5aaea4d537a56a649ff424c0a1bef
Nicolas Carrillo <n.carrillo01@ufromail.cl>
2022-06-29 14:59:18 -0400

    update#

--- radial_distortion_old.py
+++ radial_distortion_old.py
@@ -1,31 +1,34 @@
+import numpy as np
+import cv2 as cv
+import glob
+
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-a,b=9,6
+a,b=6,9
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*7,3), np.float32)
 objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
-images = glob.glob('img/images/far/*.jpg')
+
+images = glob.glob('img/images/*.jpg')
+
 for fname in images:
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(img, (a,b), None)
+    ret, corners = cv.findChessboardCorners(gray, (a,b), None)
     # If found, add object points, image points (after refining them)
-    print(ret)
     if ret == True:
         objpoints.append(objp)
         corners2 = cv.cornerSubPix(gray,corners, (a,b), (-1,-1), criteria)
         imgpoints.append(corners)
-        # Draw and display the corners
-        cv.drawChessboardCorners(img, (a,b), corners2, ret)
-        cv.imshow('img '+str(fname), cv.resize(img,(600,400)))
-        cv.waitKey(100)
-cv.destroyAllWindows()
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
+print(np.size(objpoints),np.size(imgpoints))
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
 img = cv.imread('img/chessboard/patron_1.jpg')
 h,  w = img.shape[:2]
